2492703862353d2c8449170394d81304880d5046
garlicOS® <14845347+Grosserly@users.noreply.github.com>
2019-03-27 13:00:10 -0500

    Refactor
    I wrote this code (minus that function, I got that off StackOverflow) when I was just starting out. I've learned a lot about JavaScript, modern features, and code formatting since then, so I just went back over this to better match how I program now.

---
+++ player/js/setup-pt1.js
@@ -0,0 +1,32 @@
+/**
+ *  SWF Player Setup
+ *  1. Get GET variables to use with the SWF player
+ *  2. Decode and treat variables
+ *  3. Set the page title to the game title
+ */
+
+// 1.
+function getURLVars() {
+	/**
+	Get URL Vars
+	Returns a specified variable from the URL
+	*/
+	let vars  = {};
+	let parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,
+						function(m, key, value) {
+							vars[key] = value;
+						});
+	return vars;
+}
+
+let vars = getURLVars();
+let swfTitle = vars.title;
+let swfID    = vars.id;
+
+// 2.
+swfTitle = decodeURIComponent(gameTitle); // Decode URI (i.e. %20 --> space character) 
+swfID = "swf/" + gameID + ".swf";         // Turn gameID into path to .swf
+
+// 3.
+document.title = swfTitle;
+//alert("Title: " + gameTitle + "\nID: " + gameID);
